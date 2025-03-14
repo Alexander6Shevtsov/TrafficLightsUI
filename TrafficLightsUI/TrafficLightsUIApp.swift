41924e020ab49151e00f7e87b13bc03d082a4f63
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-14 14:30:31 +0300

    Initial Commit

---
+++ TrafficLightsUI/TrafficLightsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsUIApp.swift
+//  TrafficLightsUI
+//
+//  Created by Alexander Shevtsov on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
