ade86f423a1209d2524997431d8ea42231d6eefe
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-14 16:10:22 +0300

    Add StartButtonView

---
+++ TrafficLightsUI/StartButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  StartButtonView.swift
+//  TrafficLightsUI
+//
+//  Created by Alexander Shevtsov on 14.03.2025.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartButtonView()
+}
